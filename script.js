e0d6b1144951196a54063bdbe0c28d6f3bb2f632
vishal <Vishalgehlot334@gmail.com>
2023-03-23 16:47:16 +0530

    ok

---
+++ script.js
@@ -0,0 +1,87 @@
+
+//api 
+// https://jsonplaceholder.typicode.com/todos/1
+
+
+
+// varible == undefined
+// let x='';
+
+
+
+//fetch API option methode promise method:
+
+/// single threaded : async function: 
+
+// fetch("https://jsonplaceholder.typicode.com/todos/100")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     x = data
+//     DOMMan(x.title)
+// })
+//   .catch((err)=>console.log(err))
+
+
+
+// this function takes a param/arg that argument is added to h1 and appned to the body section
+//   const DOMMan=(data)=>{
+
+//     const h1 = document.createElement('h1')
+
+//     h1.innerText=data;
+
+//     h1.setAttribute('class',"mainh1")
+//     const body = document.body
+
+// body.append(h1);
+
+//   }
+
+
+
+
+// console.log('hello')
+
+
+// TWO phases when we are scripting / coding in JS:
+// Memory allocation   
+// execution  context 
+
+
+/// Two type Stack memory HEap memory :
+
+
+// stack memory stack FILO queue it will be executed by that self 
+// HEap memory like SetTimout : API uses HEap Memory - 
+
+
+//two types of function normal function / argument function
+
+
+
+
+// const add =()=>{
+//     console.log('hello')
+// }
+
+// add()
+
+// arg/params passing function
+// const remove=(a,b)=>{
+
+// console.log(a+b)
+
+// }
+
+
+// remove(12,12)
+
+
+// API/ Function 
+//Heap Memory and stack memory 
+
+
+
+
+
+    
